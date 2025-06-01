2d21c5db5323f9d5921b2be4bdd22ae961a998df
MAXSTEEL003 <tejasadinarayan@gmail.com>
2025-06-01 16:08:08 +0530

    Resolve merge conflicts

--- next.config.js
+++ next.config.js
@@ -3,38 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  // Disable ESLint during build if you're having issues
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Disable TypeScript type checking during build to avoid the error
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Mark browser-only packages as external during server-side rendering
-      config.externals = [...(config.externals || []), 
-        'jspdf', 
-        'jspdf-autotable', 
-        'xlsx',
-        'html2canvas'
-      ];
-    }
-    
-    return config;
   }
 };
 
 module.exports = nextConfig;
-
-
-
-
-
-
-
-
-
 
 
